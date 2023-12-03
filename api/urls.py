9f3effcdda8989962545dfeb07e850b5aa2a22ba
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2023-12-03 22:49:22 +0530

    installed new app named api

--- api/urls.py
+++ api/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.getAllLinks, name='home'),
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('', views.apiHome, name='home'),
 ]
